9aa61eccfe4198aa11c5fabe97df977d2a1af11b
Klaatu98 <arabson1998@gmail.com>
2020-08-01 21:58:43 +0400

    chenges bcs of merge

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,5 @@
 
 import BaseLayout from "components/base-layout";
-<<<<<<< HEAD
-=======
-
->>>>>>> 639e05965880b6f953c36b0af837c788ac589d63
 import Hero from "components/index_components/hero";
 import Content from "components/index_components/main_content";
 
