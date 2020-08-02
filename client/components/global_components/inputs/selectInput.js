fd136caf83d68b0e7c0263483bf3e8d3ae6c9546
Klaatu98 <arabson1998@gmail.com>
2020-08-02 20:51:24 +0400

    :beer: add first draft of animation on select arrow
    used framer

--- client/components/global_components/inputs/selectInput.js
+++ client/components/global_components/inputs/selectInput.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { motion } from "framer-motion"
 
-    export default function selectInput() {
-return (
-    <div>
+
+export default function selectInput() {
+    const [toggle, setToggle] = useState(false);
+
+    const handleSelect = () => {
+        setToggle((toggle) => !toggle);
+    } 
+
+   
+    return  (
+        <div>
 
 
 {/* <select className="select_field" name="cars" id="cars">
@@ -16,9 +25,29 @@ return (
 <div className="dropdown">
       <div className="dropdown-select">
         <span className="select paragraph-regular-Noto paragraph-small">რეიტინგით</span>
-        <i className="fa fa-caret-down icon"></i>
+        {/* <i className="fa fa-caret-down icon"></i> */}
+        {toggle ?   
+           <motion.img 
+           src="./pictures/select_arrow.svg" 
+           alt="arrow" 
+           onClick={handleSelect}
+           animate={{ rotate: 180 }}
+           transition={{ duration: .3 }}
+          />
+          : 
+          <motion.img 
+             src="./pictures/select_arrow.svg" 
+            alt="arrow" 
+            onClick={handleSelect}
+            animate={{ rotate: 0 }}
+            transition={{ duration: .3 }}
+         />
+    
+    
+        }
+     
       </div>
-      <div className="dropdown-list">
+      <div className={toggle ? "dropdown-list dropdown_animation" : "dropdown-list "}>
         <div className="dropdown-list__item">Pepsi</div>
         <div className="dropdown-list__item">Coca</div>
         <div className="dropdown-list__item">Monster</div>
