cce8cfaaa663e6ee082c4b89483213941970c295
Robinzon100 <robinzon.sokhashvili@gmail.com>
2021-04-27 22:35:49 +0400

    :rocket: small changes

--- server/schemas/categories/categories.schema.ts
+++ server/schemas/categories/categories.schema.ts
@@ -6,7 +6,7 @@ import { email, arrayStringWithIds, passwordUnencrypted } from './../utils';
 export const regsiterUserSchema = Joi.object({
     full_name: Joi.string().min(6).max(225).required().trim(),
     email: email(Joi),
-    password: passwordUnencrypted(Joi),
+    password: passwordUnencrypted(),
     recovery_email: email(Joi),
     favorite_main_category_ids: arrayStringWithIds(Joi),
     favorite_sub_category_ids: arrayStringWithIds(Joi)
