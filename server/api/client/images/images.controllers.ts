220b57d293ce7931817b8c5fd1a9e9ba19fb4459
Robinzon100 <robinzon.sokhashvili@gmail.com>
2021-06-20 18:37:00 +0400

    :bug: fixed the aws s3 image problem ...... I think

--- server/api/client/images/images.controllers.ts
+++ server/api/client/images/images.controllers.ts
@@ -6,8 +6,14 @@ export const getImage = async (req: Request, res: Response, next: NextFunction) 
     try {
         const { key } = req.params
         if (key) {
-            const readStream = await getFileStream(key)
-            readStream.pipe(res)
+            try {
+                const readStream = await getFileStream(key)
+                readStream.pipe(res)
+            } catch (err: any) {
+                res.json({
+                    error: err.message
+                })
+            }
         }
     } catch (err: any) {
         customError(res, next, err.messagem, 404)
